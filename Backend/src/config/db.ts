e2dbe346713bd006da3ae050a6ed0c98f445e236
Hugo <hugopalominogalindo974@gmail.com>
2024-12-23 00:18:07 -0500

    se agregaron constantes

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../constants/env";
 
+/**@Define una funcion que ejecutara condigo asyncrono*/
+/**@await detiene la ejecucion de una funcion hasta que una promesa sea resuelta*/
 const connecToDatabase = async () => {
 	try {
+		/**@promesa */
+		/**la funcion devuelve una promesa que se
+		resuelve cuando la conexion es exitosa**/
 		await mongoose.connect(MONGO_URI);
-
+		console.log("Conexion ala base de datos exitoso");
 	} catch (error) {
 		console.log("No se pudo conectar a la base de datos", error);
 		process.exit(1);
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,28 @@
+import "dotenv/config"
 import express from 'express';
+import connecToDatabase from './config/db';
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app  = express();
 
+app.use(express.json());
+
+app.use(
+	express.urlencoded({extended: true})
+);
+
+app.use(cookieParser())	
+
+app.use(
+	cors({
+		origin: APP_ORIGIN,
+		credentials: true,
+	})
+)
+
+//app.use(errorHandler);
 
 app.get("/", (req, res) => {
 	res.status(200).json({
@@ -9,6 +30,7 @@ app.get("/", (req, res) => {
 	})
 });
 
-app.listen(4004, () => {
-	console.log('Servidor ejecutandose en el puerto 4004');
+app.listen(4004, async () => {
+	console.log(`Servidor ejecutandose en el puerto ${PORT}, ${NODE_ENV} enviroment.`);
+	await connecToDatabase();
 }) 
